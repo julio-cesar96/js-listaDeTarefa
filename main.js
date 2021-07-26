8a9d66b15c57ea371e90b8ed74562a9ca2c3a9c0
julio-cesar96 <julio_cbs96@hotmail.com>
2021-07-26 12:15:34 -0300

    Adicionando data as tarefas

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
 import BotaoConcluir from "./componentes/ConcluirTarefa.js";
 import BotaoApagar from "./componentes/ApagarTarefa.js";
-    
-    const criarTarefa = (evento) => {
-    
-        evento.preventDefault();
 
-        const lista = document.querySelector('[data-lista-tarefas]')
-        const input = document.querySelector('[data-form-input]');
-        const valorInput = input.value;
+const NovoItem = (evento) => {
+    evento.preventDefault();
+    
+    const lista = document.querySelector('[data-lista-tarefas]')
+    const input = document.querySelector('[data-form-input]');
+    const valorInput = input.value;
+    
+    const calendario = document.querySelector('[data-form-date]');
+    const data = moment(calendario.value);
+    const dataFormatada = (data.format('DD/MM/YYYY'));
 
-        const tarefa = document.createElement('li');
-        tarefa.classList.add('task');
+    const dados = {
+        valorInput,
+        dataFormatada
+    };
+
+    const criaTarefa =  criarTarefa(dados);
+    
+    lista.appendChild(criaTarefa);
+    
+    input.value = " ";
+}
+
+const criarTarefa = ({ valorInput, dataFormatada }) => {
+    const tarefa = document.createElement('li');
+    tarefa.classList.add('task');
         
-        const conteudo = `<p class="content">${valorInput}</p>`;
+    const conteudo = `<p class="content">${dataFormatada} * ${valorInput}</p>`;
 
-        tarefa.innerHTML = conteudo;
+    tarefa.innerHTML = conteudo;
 
-        tarefa.appendChild(BotaoConcluir());
-        tarefa.appendChild(BotaoApagar());
-        lista.appendChild(tarefa);
+    tarefa.appendChild(BotaoConcluir());
+    tarefa.appendChild(BotaoApagar());
 
-        input.value = " ";
-    }
+    return tarefa;
+}
 
-    const novaTarefa = document.querySelector('[data-form-button]');
-    novaTarefa.addEventListener('click', criarTarefa);
+const novaTarefa = document.querySelector('[data-form-button]');
+novaTarefa.addEventListener('click', NovoItem);
